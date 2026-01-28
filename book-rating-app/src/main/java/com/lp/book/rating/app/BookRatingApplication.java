e6a5192eac95c5fff18e060fdaa77f43010c7f0f
HCG\Lidija.Pobolovec <lidija.pobolovec@embedit.com>
2026-01-28 13:24:46 +0100

    app init

---
+++ book-rating-app/src/main/java/com/lp/book/rating/app/BookRatingApplication.java
@@ -0,0 +1,12 @@
+package com.lp.book.rating.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookRatingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BookRatingApplication.class, args);
+    }
+}
---
+++ book-rating-app/src/test/java/com/lp/book/rating/app/BookRatingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lp.book.rating.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class BookRatingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
