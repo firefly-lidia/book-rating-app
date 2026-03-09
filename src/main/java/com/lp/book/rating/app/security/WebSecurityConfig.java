80dc0b609e30e8d781254a10bae77f601eb3baa2
HCG\Lidija.Pobolovec <lidija.pobolovec@embedit.com>
2026-03-09 14:36:16 +0100

    finished JWT security

--- src/main/java/com/lp/book/rating/app/security/WebSecurityConfig.java
+++ src/main/java/com/lp/book/rating/app/security/WebSecurityConfig.java
@@ -6,12 +6,14 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 import org.springframework.http.HttpMethod;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
+import org.springframework.security.oauth2.server.resource.authentication.JwtGrantedAuthoritiesConverter;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Import({UserDetailServiceConfig.class, JwtConfig.class})
@@ -21,7 +23,8 @@ import org.springframework.security.web.SecurityFilterChain;
 public class WebSecurityConfig {
 
     @Bean
-    public SecurityFilterChain firstSecurityFilterChain(HttpSecurity http) throws Exception {
+    public SecurityFilterChain firstSecurityFilterChain(HttpSecurity http, JwtDecoder jwtDecoder,
+                                                        JwtAuthenticationConverter jwtAuthConverter) throws Exception {
         http
             .securityMatcher("/api/**")
             .csrf(AbstractHttpConfigurer::disable)
@@ -32,10 +35,20 @@ public class WebSecurityConfig {
                 .requestMatchers(HttpMethod.GET, "/api/v1/books/**").permitAll()
                 .anyRequest().authenticated()
             )
-            .httpBasic(Customizer.withDefaults());
+            .oauth2ResourceServer(oauth -> oauth.jwt(
+                jwt -> jwt.decoder(jwtDecoder).jwtAuthenticationConverter(jwtAuthConverter)));
 
         return http.build();
     }
 
+    @Bean
+    public JwtAuthenticationConverter jwtAuthConverter() {
+        var converter = new JwtAuthenticationConverter();
+        var grantAuthConverter = new JwtGrantedAuthoritiesConverter();
+        grantAuthConverter.setAuthoritiesClaimName("roles");
+        grantAuthConverter.setAuthorityPrefix("ROLE_");
+        converter.setJwtGrantedAuthoritiesConverter(grantAuthConverter);
+        return converter;
+    }
 
 }
